323ebad62733fd84358a4cda40ccefc3dcbd4bde
Aankirz <sahuankit453@gmail.com>
2023-06-13 04:16:00 +0530

    okupd

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
+     <div id="yazı">█ █ █ <span>█ █ █ █ █ █ █ █ █ █ </span>31%
+      <br/>&gt; HACKODISHA 3.0&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <br/> COMING SOON 🚀 <span id="imleç">█</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    </div>
+    </main>
+  )
+}
