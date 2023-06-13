614df21b0eb4ad7a5b43e046ea339df442363aa1
Aankirz <sahuankit453@gmail.com>
2023-06-13 21:02:19 +0530

    okupd

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black md:text-2xl">
      <div id="yazı">█ █ █ <span>█ █ █ █ █ █ █ █ █ █ </span>31%
       <br/>&gt; HACKODISHA 3.0&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       <br/> COMING SOON 🚀 <span id="imleç">█</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
