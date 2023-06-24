9bee994c11a7f4e79b4cec0531be350d53a24f3d
8rxn <rajxryn@gmail.com>
2023-06-25 00:07:55 +0530

    Update Wait Landing
    Signed-off-by: 8rxn <rajxryn@gmail.com>

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,36 @@
-import Image from 'next/image'
+"use client"
+
+import Lottie from "react-lottie";
+import animationData from "@/animations/building.json"
 
 export default function Home() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black md:text-2xl">
-     <div id="yazı">█ █ █ <span>█ █ █ █ █ █ █ █ █ █ </span>31%
-      <br/>&gt; HACKODISHA 3.0&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      <br/> COMING SOON 🚀 <span id="imleç">█</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    </div>
+    <main className="flex min-h-screen min-w-screen flex-col items-center justify-between p-24 md:text-2xl bg-blue-100">
+      <div className="drop-shadow-xl flex-col flex gap-20 items-center ">
+        <Lottie options={defaultOptions} height={400} width={400} style={{maxHeight:"90vw",maxWidth:"90vw"}} />
+      <div>
+      <h1 className="text-center font-black text-5xl break-keep break-words mb-4">HackOdisha 3.0</h1>
+      <h2 className="text-center font-black text-3xl break-keep break-words">Coming Soon 🚀</h2>
+      </div>
+      <a href="https://twitter.com/hackodisha" target="_blank" rel="noreferrer">
+      <button className="px-4 py-2 bg-blue-200 w-fit hover:bg-blue-300 ease-in duration-100 rounded-lg ">
+        <span className="inline-flex items-center gap-4">
+        Check Status 
+        <svg viewBox="0 0 24 24" aria-hidden="true" className="w-4 h-4"><g><path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path></g></svg>
+        </span>
+      
+      
+       </button>
+      </a>
+      </div>
     </main>
-  )
+  );
 }
