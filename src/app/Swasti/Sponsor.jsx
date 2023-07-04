53ef9504ab2b589c75745424f258d5c1c04abf96
SWASTI SUNDAR PRADHAN <swastipradhan044@gmail.com>
2023-07-04 18:55:45 +0530

    setup the app and started the buliding of sponsor page

---
+++ src/app/Swasti/Sponsor.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./Sponser.css"
+import Img1 from "./Assets/1.png"
+const Sponsor = () => {
+  return (
+    <>
+    <div>
+    <div className="e-loadholder">
+	<div className="m-loader">
+		<div className="e-text">
+        Sponsor Img
+        </div>
+	</div>
+     </div>
+     <div className="particleCanvas-Blue"></div>
+     <div className="particleCanvas-White"></div>
+    </div>
+    </>
+  )
+}
+
+export default Sponsor
