31406bd7da1d3d1e2ca2a1a199b29a18c74c6abc
Swadhin <nayak.swadhin25@gmail.com>
2023-07-05 20:06:49 +0530

    feat: responsive navbar

--- src/app/swadhin/components/Navbar.js
+++ src/app/swadhin/components/Navbar.js
@@ -1,30 +1,67 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 function Navbar() {
+  const [Navbar, setNavbar] = useState(false);
   return (
-    <nav className=" bg-black bg-opacity-50 w-full h-full flex justify-between items-center sticky top-0 text-white ">
-      <Link href="/swadhin/#home">
-        <Image
-          src={"/HO3.png"}
-          alt="logo"
-          width="100"
-          height="100"
-          className="p-3 pl-16 h-12 w-40"
-        />
-      </Link>
-      <ul className="flex space-x-3 items-center justify-center pr-4">
+    <nav className=" bg-navbg w-full h-full md:flex justify-between items-center sticky top-0 text-white backdrop-blur-sm">
+      <div className="flex justify-between items-center">
         <Link href="/swadhin/#home">
-          <li className="p-3 font-navtxt text-xl">Home</li>
+          <Image
+            src={"/images/HO3.png"}
+            alt="logo"
+            width="100"
+            height="100"
+            className="flex justify-center items-center p-3 md:pl-16 pl-8 w-28 h-14 md:w-40"
+          />
+        </Link>
+        <button
+          onClick={() => setNavbar(!Navbar)}
+          className="pr-8 md:hidden flex items-center justify-center"
+        >
+          {Navbar ? (
+            <Image
+              src={"/images/closeIcon.png"}
+              alt="close Icon"
+              height="30"
+              width="30"
+            />
+          ) : (
+            <Image
+              src={"/images/BurgerIcon.png"}
+              alt="Burger Icon"
+              height="30"
+              width="30"
+            />
+          )}
+        </button>
+      </div>
+      <ul
+        className={`md:flex  md:space-x-3 items-center justify-center md:pr-4 ${
+          Navbar ? "block" : "hidden"
+        }`}
+      >
+        <Link href="/swadhin/#home">
+          <li className="p-3 md:pl-3 pl-8 font-navtxt text-xl hover:bg-slate-400 hover:bg-opacity-10">
+            Home
+          </li>
         </Link>
         <Link href="/swadhin/#about">
-          <li className="p-3 font-navtxt text-xl">About Us</li>
+          <li className="p-3 md:pl-3 pl-8 font-navtxt text-xl hover:bg-slate-400 hover:bg-opacity-10">
+            About Us
+          </li>
         </Link>
         <Link href="/swadhin/#contact">
-          <li className="p-3 font-navtxt text-xl">Contact Us</li>
+          <li className="p-3 md:pl-3 pl-8 font-navtxt text-xl hover:bg-slate-400 hover:bg-opacity-10">
+            Contact Us
+          </li>
         </Link>
         <Link href="/swadhin/#event">
-          <li className="p-3 font-navtxt text-xl">Events</li>
+          <li className="p-3 md:pl-3 pl-8 font-navtxt text-xl hover:bg-slate-400 hover:bg-opacity-10">
+            Events
+          </li>
         </Link>
       </ul>
     </nav>
