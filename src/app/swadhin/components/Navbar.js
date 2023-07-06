80e0624decba4e435d375f9f28d2268a26c0ba3e
Swadhin <nayak.swadhin25@gmail.com>
2023-07-06 18:15:00 +0530

    added animation in nav bar

--- src/app/swadhin/components/Navbar.js
+++ src/app/swadhin/components/Navbar.js
@@ -7,11 +7,15 @@ import { useState } from "react";
 function Navbar() {
   const [navbar, setNavbar] = useState(false);
   return (
-    <nav className=" bg-navbg w-full h-full md:flex justify-between items-center sticky top-0 text-white backdrop-blur-sm">
+    <nav
+      className={`${
+        navbar ? "animate-trans-navbarbg" : "animate-none"
+      } bg-navbg w-full h-full md:flex justify-between items-center sticky top-0 text-white backdrop-blur-sm`}
+    >
       <div className="flex justify-between items-center">
         <Link href="/swadhin/#home">
           <Image
-            src={"/Images/HO3.png"}
+            src={"/Images/HO3.svg"}
             alt="logo"
             width="100"
             height="100"
@@ -19,7 +23,11 @@ function Navbar() {
           />
         </Link>
         <span className="pr-8 md:hidden flex items-center justify-center">
-          <Hamburger toggled={navbar} toggle={() => setNavbar(!navbar)} />
+          <Hamburger
+            toggled={navbar}
+            toggle={() => setNavbar(!navbar)}
+            duration={0.5}
+          />
         </span>
       </div>
       <ul
@@ -28,22 +36,22 @@ function Navbar() {
         }`}
       >
         <Link href="/swadhin/#home">
-          <li className="p-3 md:pl-3 pl-8 font-navtxt text-xl hover:bg-slate-400 hover:bg-opacity-10">
+          <li className="animate-trans-navbar-component-1 md:animate-none p-3 md:pl-3 pl-8 font-navtxt text-xl hover:bg-slate-400 hover:bg-opacity-10">
             Home
           </li>
         </Link>
         <Link href="/swadhin/#about">
-          <li className="p-3 md:pl-3 pl-8 font-navtxt text-xl hover:bg-slate-400 hover:bg-opacity-10">
+          <li className="animate-trans-navbar-component-2 md:animate-none p-3 md:pl-3 pl-8 font-navtxt text-xl hover:bg-slate-400 hover:bg-opacity-10">
             About Us
           </li>
         </Link>
         <Link href="/swadhin/#contact">
-          <li className="p-3 md:pl-3 pl-8 font-navtxt text-xl hover:bg-slate-400 hover:bg-opacity-10">
+          <li className="animate-trans-navbar-component-3 md:animate-none p-3 md:pl-3 pl-8 font-navtxt text-xl hover:bg-slate-400 hover:bg-opacity-10">
             Contact Us
           </li>
         </Link>
         <Link href="/swadhin/#event">
-          <li className="p-3 md:pl-3 pl-8 font-navtxt text-xl hover:bg-slate-400 hover:bg-opacity-10">
+          <li className="animate-trans-navbar-component-4 md:animate-none p-3 md:pl-3 pl-8 font-navtxt text-xl hover:bg-slate-400 hover:bg-opacity-10">
             Events
           </li>
         </Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,8 +16,19 @@ module.exports = {
       fontFamily: {
         navtxt: "'myfont',sans-serif",
       },
+      keyframes: {
+        "trans-navbar": {
+          from: { opacity: "0", transform: "translateX(-2%)", width: "95%" },
+          to: { opacity: "1", transform: "translateX(0)", width: "90%" },
+        },
+      },
+      animation: {
+        "trans-navbar-component-1": "trans-navbar 0.25s ease-in-out 1",
+        "trans-navbar-component-2": "trans-navbar 0.5s ease-in-out 1",
+        "trans-navbar-component-3": "trans-navbar 0.75s ease-in-out 1",
+        "trans-navbar-component-4": "trans-navbar 1s ease-in-out 1",
+      },
       backgroundImage: {
-        homebg: "url('/homebg.png')",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
