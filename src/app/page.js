4ebb625a9f2676b4d264f13143177d7147b0aa24
Om Prasad <omprasadsahu9090@gmail.com>
2023-07-07 17:57:30 +0530

    Add background image

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,10 @@
 import Player from "@/components/Player";
 import { useState } from "react";
 import Timeline from "./omprasad/timeline";
-import Test2 from "./omprasad/test";
 export default function Home() {
-
   return (
     <>
-    {/* <h1>Hello World</h1> */}
-    <Test2/>
+    <Timeline/>
     </>
   );
 }
