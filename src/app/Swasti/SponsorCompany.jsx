77d1b4f979dee46bde2fa7445ece7296b070ddc2
SWASTI SUNDAR PRADHAN <swastipradhan044@gmail.com>
2023-07-09 16:29:13 +0530

    merged all css files and made some major changes

--- src/app/Swasti/SponsorCompany.jsx
+++ src/app/Swasti/SponsorCompany.jsx
@@ -2,32 +2,31 @@ import React from 'react'
 import image1 from "./Assets/1.svg"
 import image2 from "./Assets/2.svg"
 import image3 from "./Assets/3.svg"
-
-import "./SponsorCompany.css"
+import "../globals.css"
+import "./Sponsor.css"
 import Image from 'next/image'
 const SponsorCompany = () => {
-  const images = [image1, image2, image3,image1, image2, image3,image1, image2, image3 ]
+  const images = [image1, image2, image3, image1, image2, image3, image1, image2, image3]
   return (
     <>
-    <section className='main-section'>
-    
-    {
-      images.map((image, index) => {
-        return (
-          <div className='Big-dot' key={index}>
-            <div className='Mid-dot'>
-              <div className='Small-dot'>
-                <div className='container'>
-                  <div className="item">
-                    <Image src={image} alt="" />
+      <section className='main-section overflow-hidden grid grid-cols-3 md:min-h-full'>
+        {
+          images.map((image, index) => {
+            return (
+              <div className='Big-dot flex justify-center w-24 h-24 m-4 sm:w-36 sm:h-36 sm:m-10 md:w-56 md:h-56 md:m-14 xl:h-60 xl:w-60' key={index}>
+                <div className='Mid-dot flex justify-center w-20 h-20 sm:w-28 sm:h-28 md:w-40 md:h-40'>
+                  <div className='Small-dot flex justify-center w-24 h-24 '>
+                    <div className='container flex w-16 h-16 justify-center md:w-32 md:h-32'>
+                      <div className="item w-10 h-10 flex justify-center md:w-16 md:h-14 sm:w-10 sm:h-10">
+                        <Image src={image} alt="xyz" />
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
-        )
-      })
-    }
+            )
+          })
+        }
       </section>
 
     </>
--- src/app/Swasti/SponsorPage.jsx
+++ src/app/Swasti/SponsorPage.jsx
@@ -1,54 +1,45 @@
 import React from 'react'
 import SponsorCompany from './SponsorCompany'
-import Text from './Text'
 import "./Sponsor.css"
-import Image from 'next/image';
+import Silver from "./Assets/silver.svg"
+import Bronze from "./Assets/bronze.svg"
 import photo from "./Assets/Group 143.svg"
 const SponsorPage = () => {
   return (
     <>
-      <section className='main-sponsor'>
-        <section className='first mt-5 pt-32'>
-          <section className='mb-40 '>
-            <div className='flex flex-col md:flex-row md:justify-between'>
-              <div className='md:mt-80 md:-ml-96 mb-20'>
-                <Text cat="SILVER" />
-              </div>
-              <div>
-                <Image src={photo} alt="" className=' hidden md:flex' />
-              </div>
-              <div className="md:mr-16">
-                <SponsorCompany />
-                
-              </div>
-            </div>
-          </section>
-        </section>
-        <section className='md:hidden'>
-          <div className='flex flex-col md:flex-row md:justify-between'>
-            <div className='md:mt-80 md:-ml-4 mb-20 '>
-              <Text cat="BRONZE" />
-            </div>
-            <div className="md:-ml-96">
-              <SponsorCompany />
+      <section className='overflow-hidden main-sponsor'>
+        <div className='sm:hidden md:hidden xl:hidden 2xl:hidden flex text-[#CA40CD] justify-center font pt-4'>
+          SILVER SPONSORS
+        </div>
+        <section className='flex flex-row md:flex-row md:justify-between h-max first md:mt-3 md:mr-2'>
+          <div className=''>
+            <div className='md:h-max md:w-max md:flex md:ml-10 hidden md:mt-28'>
+              <img src={Silver.src} alt="silver" />
             </div>
           </div>
+          <div className='pic md:ml-24 md:mt-28 '>
+            <img src={photo.src} alt="" className='hidde/n md:flex h-[35rem] md:h-max ' />
+          </div>
+          <div className='mt-8 md:mt-20'>
+            <SponsorCompany />
+          </div>
         </section>
-        <section className='second mt-5 pt-32'>
-        <section className='hidden md:flex'>
-          <div className='flex flex-col md:flex-row md:justify-between'>
-            <div className="md:ml-16">
-              <SponsorCompany />
+        <div className='sm:hidden md:hidden flex text-[#CA40CD] justify-center font pt-4'>
+          BRONZE SPONSORS
+        </div>
+        <section className='flex flex-row md:flex-row md:justify-between h-max first md:mt-32 md:mr-2 second'>
+          <div className='mt-3 mr-4 md:mt-20'>
+            <SponsorCompany />
+          </div>
+          <div className='pic md:mr-14 md:mt-20 -mt-8 md:rotate-180 rotate-180'>
+            <img src={photo.src} alt="" className='hidde/n md:flex h-[35rem] md:h-max' />
+          </div>
+          <div className=''>
+            <div className='hidden md:flex md:mr-10 md:mt-32 '>
+              <img src={Bronze.src} alt="silver" />
             </div>
-            <div className=''>
-            <Image src={photo} alt="" className='rotate-180' />
+          </div>
 
-            </div>
-            <div className='md:mt-80 md:ml-4 mb-20'>
-              <Text cat="BRONZE" />
-            </div>
-          </div>
-        </section>
         </section>
       </section>
     </>
