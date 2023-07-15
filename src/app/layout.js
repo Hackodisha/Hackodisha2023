6724c16c179eeac67ccf58642dc46305e5693fb6
Srishty Mangutte <srishtymangutte@gmail.com>
2023-07-15 14:15:10 +0530

    feat: added styling

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
+import { AboutUs } from '@/components/AboutUs';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <AboutUs/>
     </html>
   )
 }
--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import {Logo} from './Images/HackLogo.png';
+import Logo from './Images/HackLogo.svg';
 
 export const AboutUs = () => {
   return (
-    <div className='aboutus'>
+    <div className='aboutus flex flex-row'>
         <div className='logo-container'>
             <img src={Logo} className='hacklogo' />
         </div>
         <div className='para-container'>
-        HackOdisha- a thrilling 36-hour online hackathon organized by Webwiz, Nit Rourkela— an event dedicated to fostering community collaboration. With an impressive turnout of over 1600 participants across India, this hackathon promises to be a platform where innovation knows no bounds.
+        HackOdisha- a thrilling <span className='pink-span'>36-hour online hackathon</span> organized by <span className='pink-span'>Webwiz, Nit Rourkela</span>— an event dedicated to fostering community collaboration. With an impressive turnout of over 1600 participants across India, this hackathon promises to be a platform where innovation knows no bounds.
 
         Here, we witness groundbreaking projects spanning many fields, including healthcare, education, environment, women's security, and much more. Our participants bring their unique perspectives and expertise to address real-world challenges in these critical areas.
 
