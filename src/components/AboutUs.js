ef3744bfcbb1191ec948efcefe6da201daeb3b06
Srishty Mangutte <srishtymangutte@gmail.com>
2023-07-15 13:47:12 +0530

    feat:added paragraph

---
+++ src/components/AboutUs.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const AboutUs = () => {
+  return (
+    <div className='aboutus'>
+        <div className='logo-container'>
+
+        </div>
+        <div className='para-container'>
+        HackOdisha- a thrilling 36-hour online hackathon organized by Webwiz, Nit Rourkela— an event dedicated to fostering community collaboration. With an impressive turnout of over 1600 participants across India, this hackathon promises to be a platform where innovation knows no bounds.
+
+        Here, we witness groundbreaking projects spanning many fields, including healthcare, education, environment, women's security, and much more. Our participants bring their unique perspectives and expertise to address real-world challenges in these critical areas.
+
+        We celebrate the power of technology and the indomitable spirit of our participants. Together, we're shaping a brighter future through innovation and collaboration.
+
+        </div>
+    </div>
+  )
+}
