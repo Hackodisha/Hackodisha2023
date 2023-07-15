9c15e6b1af524ec67f0a840fdd4bd9f23572eb3f
Srishty Mangutte <srishtymangutte@gmail.com>
2023-07-16 00:18:07 +0530

    fix: chaged css

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,18 +1,20 @@
 import React from 'react';
+import Image from "next/image";
+import './styles/aboutus.styles.css'
 import Logo from './Images/HackLogo.svg';
 
 export const AboutUs = () => {
   return (
-    <div className='aboutus flex flex-row'>
+    <div className='aboutus flex flex-row bg-black px-10 py-20	text-justify justify-between '>
         <div className='logo-container'>
-            <img src={Logo} className='hacklogo' />
+            <Image src={Logo} className='hacklogo' />
         </div>
-        <div className='para-container'>
-        HackOdisha- a thrilling <span className='pink-span'>36-hour online hackathon</span> organized by <span className='pink-span'>Webwiz, Nit Rourkela</span>— an event dedicated to fostering community collaboration. With an impressive turnout of over 1600 participants across India, this hackathon promises to be a platform where innovation knows no bounds.
+        <div className='para-container w-[841px] px-8 '>
+          <p className='paras'> HackOdisha- a thrilling <span className='pink-span'>36-hour online hackathon</span> organized by <span className='pink-span'>Webwiz, Nit Rourkela</span>— an event dedicated to fostering community collaboration. With an impressive turnout of over 1600 participants across India, this hackathon promises to be a platform where innovation knows no bounds. </p> 
 
-        Here, we witness groundbreaking projects spanning many fields, including healthcare, education, environment, women's security, and much more. Our participants bring their unique perspectives and expertise to address real-world challenges in these critical areas.
+          <p className='paras'> Here, we witness groundbreaking projects spanning many fields, including healthcare, education, environment, women's security, and much more. Our participants bring their unique perspectives and expertise to address real-world challenges in these critical areas. </p> 
 
-        We celebrate the power of technology and the indomitable spirit of our participants. Together, we're shaping a brighter future through innovation and collaboration.
+          <p className='paras'> We celebrate the power of technology and the indomitable spirit of our participants. Together, we're shaping a brighter future through innovation and collaboration. </p> 
 
         </div>
     </div>
