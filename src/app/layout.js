a3dbcf5a0862adf372a129d7e79fdcb0ee38fa04
Srishty Mangutte <srishtymangutte@gmail.com>
2023-07-16 14:19:32 +0530

    fix: netlify checks

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,9 +12,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      
+      <body className={inter.className}>{children}</body> 
       <AboutUs/>
     </html>
   )
 }
-// <body className={inter.className}>{children}</body> 
