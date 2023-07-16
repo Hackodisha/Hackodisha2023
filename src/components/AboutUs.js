1f8464e2a1c14f8ed2cf325fc305f7fea5b849eb
Srishty Mangutte <srishtymangutte@gmail.com>
2023-07-16 14:34:43 +0530

    fix: eslint rules

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -18,9 +18,9 @@ export const AboutUs = () => {
         <div className='para-container w-[841px] px-8 '>
           <p className='paras'> HackOdisha- a thrilling <span className='pink-span'>36-hour online hackathon</span> organized by <span className='pink-span'>Webwiz, Nit Rourkela</span>— an event dedicated to fostering community collaboration. With an impressive turnout of over 1600 participants across India, this hackathon promises to be a platform where innovation knows no bounds. </p> 
 
-          <p className='paras'> Here, we witness groundbreaking projects spanning many fields, including healthcare, education, environment, women's security, and much more. Our participants bring their unique perspectives and expertise to address real-world challenges in these critical areas. </p> 
+          <p className='paras'> Here, we witness groundbreaking projects spanning many fields, including healthcare, education, environment, women&apos;s security, and much more. Our participants bring their unique perspectives and expertise to address real-world challenges in these critical areas. </p> 
 
-          <p className='paras'> We celebrate the power of technology and the indomitable spirit of our participants. Together, we're shaping a brighter future through innovation and collaboration. </p> 
+          <p className='paras'> We celebrate the power of technology and the indomitable spirit of our participants. Together, we&apos;re shaping a brighter future through innovation and collaboration. </p> 
 
         </div>
     </div>
