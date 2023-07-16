15d3dd93282ca3c169497d5070fa7b8c133bbeeb
Srishty Mangutte <srishtymangutte@gmail.com>
2023-07-16 14:29:37 +0530

    feat: used common-component

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -2,17 +2,18 @@ import React from 'react';
 import Image from "next/image";
 import './styles/aboutus.styles.css'
 import Logo from './Images/HackLogo.svg';
+import Title from './CommonComponent/Title/Title';
 
 export const AboutUs = () => {
   return (
     <section className='about-section  bg-black'>
     <div className='header-sec '>
-      <h1 className='heading underline xl:text-[3.5rem] px-10 py-10'>About Us</h1>
+      <Title title={"About Us"} className={"px-10 py-5"}></Title>
       <hr className='line'/>
     </div>
     <div className='aboutus flex flex-row px-10 py-20	text-justify justify-between items-center	 '>
         <div className='logo-container'>
-            <Image src={Logo} className='hacklogo' />
+            <Image src={Logo} className='hacklogo' alt='hackodisha-logo'/>
         </div>
         <div className='para-container w-[841px] px-8 '>
           <p className='paras'> HackOdisha- a thrilling <span className='pink-span'>36-hour online hackathon</span> organized by <span className='pink-span'>Webwiz, Nit Rourkela</span>— an event dedicated to fostering community collaboration. With an impressive turnout of over 1600 participants across India, this hackathon promises to be a platform where innovation knows no bounds. </p> 
