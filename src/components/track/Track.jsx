3836ee9fd8b1eaf8d8cb9e2ba5447a037017e19f
sakshi <ayushmanj138@gmail.com>
2023-07-20 15:54:32 +0530

    feat:Responsivness

--- src/components/track/Track.jsx
+++ src/components/track/Track.jsx
@@ -1,11 +1,9 @@
 function Track () {
     return (
-        <div className="bg-black grid grid-cols-3 gap-0  ">
+        <div className="bg-black grid grid-cols-1 gap-0  lg:grid-cols-3 md:grid-cols-1 ">
         
-        <div className=" items-center justify-center row-span-2 col-span-2   xl:p-0 md:text-2xl red   "
-        style={{
-           marginLeft:"330px"
-        }} >
+        <div className=" items-center justify-center p-12 lg:row-span-2 lg:col-span-2  md:text-2xl red lg:ml-80  "
+       >
         <svg xmlns="http://www.w3.org/2000/svg" width="320" height="350" viewBox="0 0 714 833" fill="none" >
 <path d="M205.666 44.5001H3V785H7.25545V829.5H460.992L508.866 783H711V3.00012H255.135L205.666 44.5001Z" stroke="#CA40CD" stroke-width="6"/>
 <text x="50%" y="30%" dominant-baseline="middle" text-anchor="middle" font-size="50" font-family="Roboto" >
@@ -20,7 +18,7 @@ function Track () {
 </div>
 
 
-<div className="items-center justify-center p-12 xl:p-0 md:text-2xl red  ">
+<div className="items-center justify-center p-12 xl:p-0 md:text-2xl red md:ml-294 ">
 <svg xmlns="http://www.w3.org/2000/svg" width="320" height="192" viewBox="0 0 671 378" fill="none" >
 <path d="M669 2.00012H2V376H436L479 327.5H669V2.00012Z" stroke="#BBB4B4" stroke-width="3"/>
 <text x="50%" y="40%" dominant-baseline="middle" text-anchor="middle" font-size="50" font-family="Roboto" >
