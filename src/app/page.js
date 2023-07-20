0e5df04e62cfec20c95a72f337ffd443dbff58e7
Sanseet <123621607+Sanseet@users.noreply.github.com>
2023-07-20 11:04:02 +0530

    Add FAQs Page (#28)
    Commit History
    * FAQs Page
    
    * updates
    
    * Update
    
    * Background update
    
    * Responsiveness fixed
    
    * background change
    
    * Faq title fixed
    
    * Some mminor issues of faq title updated
    
    * fix: build error
    
    * File structure changed
    
    * background updated
    
    * Layout modified
    
    * Some minor bug fixed
    
    * responsiveness updated
    
    * margin added
    
    * Finalise FAQ
    
    * Tweaks to FAQ
    
    * fix Build
    
    Signed-off-by: Raj Aryan <rajxryn@gmail.com>
    
    ---------
    
    Signed-off-by: Raj Aryan <rajxryn@gmail.com>
    Co-authored-by: Sanseet <sanseetsuna2004@gmail.com>
    Co-authored-by: Raj Aryan <75237697+8rxn@users.noreply.github.com>
    Co-authored-by: Raj Aryan <rajxryn@gmail.com>

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+
+import Accordion from "@/components/FAQs/Accordion";
 import Hero from "@/components/Hero/Hero";
 import Timeline from "@/components/Timeline/timeline";
 import Statistics from "../components/suraj/Statistics";
@@ -7,14 +9,16 @@ import Footer from "@/components/Footer/Footer";
 export default function Home() {
   return (
     <>
+
       <Hero />
       <AboutUs />
       <Statistics />
-      <div className="relative">
       <Timeline />
+      <div className="relative">
+      <Accordion/>
       <Footer/>
 
-      </div>
+      </div>    
     </>
   );
 }
