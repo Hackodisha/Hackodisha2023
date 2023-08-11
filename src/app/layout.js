e821c0a5e60aad5ab165310c88e9a0875d293faf
Raj Aryan <rajxryn@gmail.com>
2023-08-11 22:25:56 +0530

    Improved Keywords
    Signed-off-by: Raj Aryan <rajxryn@gmail.com>

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,21 @@ import "./globals.css";
 
 export const metadata = {
   title: "HackOdisha 3.0",
-  description: "Largest Student hackathon of Odisha | 3rd Edition | 2023",
+  description: "Largest Student hackathon of Odisha | HackOdisha 3.0 - a thrilling 36-hour online hackathon organized by Webwiz, Nit Rourkela— an event dedicated to fostering community collaboration.",
+  image: "https://hackodisha.tech/Images/HO3.svg",
+  url: "https://hackodisha.tech/",
+  keywords: [
+    "HackOdisha",
+    "HackOdisha 3.0",
+    "HackOdisha 2023",
+    "HackOdisha 3.0 2023",
+    "Hack Odisha",
+    "Hackathon",
+    "NIT Rourkela",
+    "student run hackathon",
+    "hackathon in odisha",
+    "hackathon in india",
+  ],
 };
 
 export default function RootLayout({ children }) {
