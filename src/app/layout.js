0bfc854fce306f4c97c60c6d1b1eaa7cc5ae1c5e
Raj Aryan <rajxryn@gmail.com>
2023-08-12 16:16:35 +0530

    add tag
    Signed-off-by: Raj Aryan <rajxryn@gmail.com>

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,11 @@
 import Navbar from "@/components/Navbar/Navbar";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata = {
   title: "HackOdisha 3.0",
-  description: "Largest Student hackathon of Odisha | HackOdisha 3.0 - a thrilling 36-hour online hackathon organized by Webwiz, Nit Rourkela— an event dedicated to fostering community collaboration.",
+  description:
+    "Largest Student hackathon of Odisha | HackOdisha 3.0 - a thrilling 36-hour online hackathon organized by Webwiz, Nit Rourkela— an event dedicated to fostering community collaboration.",
   image: "https://hackodisha.tech/Images/HO3.svg",
   url: "https://hackodisha.tech/",
   keywords: [
@@ -20,10 +22,41 @@ export const metadata = {
   ],
 };
 
+const jsonLd = {
+  "@context": "https://schema.org",
+  "@type": "Website",
+  name: "HackOdisha 3.0",
+  image: "https://hackodisha.tech/Images/HO3.svg",
+  description:
+    "Largest Student hackathon of Odisha | HackOdisha 3.0 - a thrilling 36-hour online hackathon organized by Webwiz, Nit Rourkela— an event dedicated to fostering community collaboration.",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-MLP0HVXXM9"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-MLP0HVXXM9', {
+            page_path: window.location.pathname,
+          });
+        `,
+          }}
+        />
         <link
           rel="icon"
           href="/Images/HO3.svg"
